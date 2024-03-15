48c1b699400f7223e4223624d8a8c2180fcefa2b
kartoffelkaese <75094938+kartoffelkaese@users.noreply.github.com>
2024-03-15 08:52:15 +0100

    Update redirect-posts.php

--- redirect-posts.php
+++ redirect-posts.php
@@ -2,10 +2,10 @@
 /*
 Plugin Name: Post/Event Redirect
 Plugin URI: https://github.com/kartoffelkaese/redirect-posts
-Description: This plugin redirects clicks on 'event' or 'post' to a user-defined URL, if provided. If not, it uses the given PERMALINK.
+Description: This plugin redirects clicks on 'post', 'event', and 'page' to a user-defined URL, if provided. If not, it uses the given PERMALINK.
 Author: Martin Urban
 Author URI: https://github.com/kartoffelkaese/
-Version: 1.0
+Version: 1.1
 */
 
 // Prevent direct access to the plugin file
@@ -14,7 +14,7 @@ if (!defined('ABSPATH')) {
 }
 
 function mcr_add_custom_box() {
-    $screens = ['post', 'event'];
+    $screens = ['post', 'event', 'page'];
     foreach ($screens as $screen) {
         add_meta_box(
             'mcr_box_id',
@@ -28,7 +28,7 @@ add_action('add_meta_boxes', 'mcr_add_custom_box');
 
 function mcr_custom_box_html($post) {
     $value = get_post_meta($post->ID, '_mcr_custom_redirect_url', true);
-    echo '<input type="text" name="mcr_custom_redirect_url" value="' . esc_attr($value) . '"/>';
+    echo '<input type="text" name="mcr_custom_redirect_url" value="'. esc_attr($value). '"/>';
 }
 
 function mcr_save_postdata($post_id) {
@@ -51,4 +51,15 @@ function mcr_redirect_to_custom_url($url, $post) {
 }
 add_filter('post_link', 'mcr_redirect_to_custom_url', 10, 2);
 add_filter('post_type_link', 'mcr_redirect_to_custom_url', 10, 2);
+function mcr_redirect_page_to_custom_url($url) {
+    $post = get_post();
+    if ($post->post_type === 'page') {
+        $custom_url = get_post_meta($post->ID, '_mcr_custom_redirect_url', true);
+        if ($custom_url) {
+            return esc_url($custom_url);
+        }
+    }
+    return $url;
+}
+add_filter('page_link', 'mcr_redirect_page_to_custom_url');
 ?>
